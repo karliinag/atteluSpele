115cbfc325b921d82314134d4fefb4eb8a0bab58
karliinag <karlina.gorbunova@gmail.com>
2023-12-07 15:13:32 +0200

    Update atteluSpele.py

---
+++ atteluSpele.py
@@ -0,0 +1,18 @@
+from tkinter import*
+from PIL import ImageTk, Image
+import random
+from tkinter import messagebox
+gameWindow=Tk()
+gameWindow.title("Vienādie attēli")
+btn0=Button(width=200,height=300)
+btn1=Button(width=200,height=300)
+btn2=Button(width=200,height=300)
+btn3=Button(width=200,height=300)
+btn4=Button(width=200,height=300)
+btn5=Button(width=200,height=300)
+btn6=Button(width=200,height=300)
+btn7=Button(width=200,height=300)
+btn8=Button(width=200,height=300)
+btn9=Button(width=200,height=300)
+
+gameWindow.mainloop()
