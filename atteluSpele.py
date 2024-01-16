3a727eb169e960921fa366dfffa114bc63c16a82
karliinag <karlina.gorbunova@gmail.com>
2024-01-16 13:38:13 +0200

    Update atteluSpele.py
    16.01.2024

--- atteluSpele.py
+++ atteluSpele.py
@@ -20,18 +20,18 @@ ImageList=[myimg1,myimg1,myimg2,myimg2,myimg3,myimg3,myimg4,myimg4,myimg5,myimg5
 
 bgImg=ImageTk.PhotoImage(Image.open("bat.jpg"))
 
-btn0=Button(width=12,height=10,image=bgImg,command=lambda:btnClick(btn0,0))
-btn1=Button(width=12,height=10,image=bgImg,command=lambda:btnClick(btn1,1))
-btn2=Button(width=12,height=10,image=bgImg,command=lambda:btnClick(btn2,2))
-btn3=Button(width=12,height=10,image=bgImg,command=lambda:btnClick(btn3,3))
-btn4=Button(width=12,height=10,image=bgImg,command=lambda:btnClick(btn4,4))
-btn5=Button(width=12,height=10,image=bgImg,command=lambda:btnClick(btn5,5))
-btn6=Button(width=12,height=10,image=bgImg,command=lambda:btnClick(btn6,6))
-btn7=Button(width=12,height=10,image=bgImg,command=lambda:btnClick(btn7,7))
-btn8=Button(width=12,height=10,image=bgImg,command=lambda:btnClick(btn8,8))
-btn9=Button(width=12,height=10,image=bgImg,command=lambda:btnClick(btn9,9))
-btn10=Button(width=12,height=10,image=bgImg,command=lambda:btnClick(btn10,10))
-btn11=Button(width=12,height=10,image=bgImg,command=lambda:btnClick(btn11,11))
+btn0=Button(width=200,height=300,image=bgImg,command=lambda:btnClick(btn0,0))
+btn1=Button(width=200,height=300,image=bgImg,command=lambda:btnClick(btn1,1))
+btn2=Button(width=200,height=300,image=bgImg,command=lambda:btnClick(btn2,2))
+btn3=Button(width=200,height=300,image=bgImg,command=lambda:btnClick(btn3,3))
+btn4=Button(width=200,height=300,image=bgImg,command=lambda:btnClick(btn4,4))
+btn5=Button(width=200,height=300,image=bgImg,command=lambda:btnClick(btn5,5))
+btn6=Button(width=200,height=300,image=bgImg,command=lambda:btnClick(btn6,6))
+btn7=Button(width=200,height=300,image=bgImg,command=lambda:btnClick(btn7,7))
+btn8=Button(width=200,height=300,image=bgImg,command=lambda:btnClick(btn8,8))
+btn9=Button(width=200,height=300,image=bgImg,command=lambda:btnClick(btn9,9))
+btn10=Button(width=200,height=300,image=bgImg,command=lambda:btnClick(btn10,10))
+btn11=Button(width=200,height=300,image=bgImg,command=lambda:btnClick(btn11,11))
 
 btn0.grid(row=1, column=0)
 btn1.grid(row=2, column=0)
@@ -49,13 +49,13 @@ btn11.grid(row=3, column=3)
 random.shuffle(ImageList)
 
 count=0
-correctAnswer=0
-Answer=[]
-Answer_dict={}
-Answercount=0
+correctanswer=0
+answer=[]
+answer_dict={}
+answercount=0
 
 def reset():
-    global count,correctAnswer, Answer, Answer_dict, Answercount
+    global count,correctanswer, answer, answer_dict, answercount
     btn0.config(state=NORMAL)
     btn1.config(state=NORMAL)
     btn2.config(state=NORMAL)
@@ -63,20 +63,20 @@ def reset():
     btn4.config(state=NORMAL)
     btn5.config(state=NORMAL)
 
-    btn0["Image"]="pyimage5"
-    btn1["Image"]="pyimage5"
-    btn2["Image"]="pyimage5"
-    btn3["Image"]="pyimage5"
-    btn4["Image"]="pyimage5"
-    btn5["Image"]="pyimage5"
+    btn0["image"]="pyimage7"
+    btn1["image"]="pyimage7"
+    btn2["image"]="pyimage7"
+    btn3["image"]="pyimage7"
+    btn4["image"]="pyimage7"
+    btn5["image"]="pyimage7"
 
     random.shuffle(ImageList)
 
     count=0
-    correctAnswer=0
-    Answer=[]
-    Answer_dict={}
-    Answercount=0       
+    correctanswer=0
+    answer=[]
+    answer_dict={}
+    answercount=0       
 
 def infoLogs():
   gameWindow=Toplevel()
@@ -87,30 +87,30 @@ def infoLogs():
   return 0
 
 def btnClick(btn,number):
-    global count, correctAnswer, Answer, Answer_dict
-    if btn["bat.jpg"]=="pyimage6" and count<2:
+    global count, correctanswer, answer, answer_dict
+    if btn["bat.jpg"]=="pyimage7" and count<2:
         btn["bat.jpg"]=ImageList[number]
     count+=1
-    Answer.append(number)
-    Answer_dict[btn]=ImageList[number]
-    if len(Answer)==2:
-        if ImageList[Answer[0]]==ImageList[Answer[1]]:
-            for key in Answer_dict:
+    answer.append(number)
+    answer_dict[btn]=ImageList[number]
+    if len(answer)==2:
+        if ImageList[answer[0]]==ImageList[answer[1]]:
+            for key in answer_dict:
                 key["state"]=DISABLED
-            correctAnswer+=2
-            if(correctAnswer==2):
+            correctanswer+=2
+            if(correctanswer==2):
                  messagebox.showinfo("Vienādi attēli, uzminēji")
-                 correctAnswer=0
-            if(correctAnswer==6):
+                 correctanswer=0
+            if(correctanswer==6):
                 messagebox.askquestion("Vienādi attēli","Tu uzvarēji, vēlies spēlēt vēlreiz?")
         else:
             messagebox.showinfo("Vienādi attēli.","neuzminēji")
-            for key in Answer_dict:
-                key["Image"]=="pyiimage6"
+            for key in answer_dict:
+                key["image"]=="pyiimage7"
     
         count=0
-        Answer=[]
-        Answer_dict={}
+        answer=[]
+        answer_dict={}
 
         return 0
  
